cfd9eeeb933440bd70616724252c74c53080d2b7
Jonathan <150624002+marte2050@users.noreply.github.com>
2025-09-24 18:26:57 -0300

    :sparkles: feat: Update my-navbaritem to remove unnecessary span wrapper around slot

--- src/components/my-navbar/my-navbaritem.tsx
+++ src/components/my-navbar/my-navbaritem.tsx
@@ -9,7 +9,7 @@ export class MyNavbaritem{
   render() {
     return (
       <Host role='listitem'>
-          <span class={`text-white font-normal text-lg`}> <slot/> </span>
+          <slot></slot>
       </Host>
     );
   }
