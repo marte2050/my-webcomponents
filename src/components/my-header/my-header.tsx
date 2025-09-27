e1a0644da2c94fa31b9f2172bfddb1cf72c997b9
Jonathan <150624002+marte2050@users.noreply.github.com>
2025-09-27 14:45:11 -0300

    style: Add fixed positioning to header in MyHeader component

--- src/components/my-header/my-header.tsx
+++ src/components/my-header/my-header.tsx
@@ -9,7 +9,7 @@ export class MyHeader {
   render() {
     return (
       <Host>
-        <header>
+        <header class={`fixed top-0 left-0 right-0`}>
           <slot/>
         </header>
       </Host>
